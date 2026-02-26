3bac9712c18e5a4594765ddad6cca6206da71405
sonic <sonic45600@gmail.com>
2026-02-26 22:53:21 +0300

    очистка

--- obj/Debug/net10.0-ios/iossimulator-x64/MauiTicTac.AssemblyInfo.cs
+++ obj/Debug/net10.0-ios/iossimulator-x64/MauiTicTac.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiTicTac")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+f3978f5d0cd6eae7c032b8d6feca014afc2cfdd1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+ed8742c626c36584a6fcacea22fe67b5633c06aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiTicTac")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiTicTac")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
